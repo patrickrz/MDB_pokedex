5dabd5725c639809b2f57a9e41ad3bd5fec81065
Patrick Zhu <Patrick>
2020-09-24 16:48:05 -0700

    the first commit

---
+++ pokedexProject/pokedex/CollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  CollectionViewCell.swift
+//  pokedex
+//
+//  Created by Patrick Zhu on 9/24/20.
+//  Copyright © 2020 Patrick Zhu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    @IBOutlet weak var profilePic: UIImageView!
+    
+    
+    func configurePFP(with imageURL: String) {
+        guard let imageUrl:URL = URL(string: imageURL) else {
+            return
+        }
+        profilePic.loadImage(withUrl: imageUrl)
+    }
+    
+    //Sets the label text to provided pokemonName
+   
+}
+
+extension UIImageView {
+    func loadImage(withUrl url: URL) {
+        DispatchQueue.global().async{ [weak self] in
+        if let data = try? Data(contentsOf: url) {
+            if let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            }
+        }
+    }
+}
+}
---
+++ pokedexProject/pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Patrick Zhu on 9/22/20.
+//  Copyright © 2020 Patrick Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
